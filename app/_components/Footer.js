652296f02533d6731270f236e468884e50651390
Amrit Reddy <amrit_reddy@me.com>
2025-03-31 18:07:56 -0600

    Update Footer.js
    Date change.

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="flex justify-center bg-gray-900 text-white h-16 rounded-md p-4 shadow-sm">
-      <p>© 2023 GameDB. All rights reserved.</p>
+      <p>© 2025 GameDB. All rights reserved.</p>
     </footer>
   );
 }
