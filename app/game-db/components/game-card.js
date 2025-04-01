6b120436ba5a5abef3a32ee1bb36a929eb2b9048
Amrit Reddy <amrit_reddy@me.com>
2025-03-31 18:29:12 -0600

    Update game-card.js
    Add conditional rendering for Steam Rating, Steam Rating Count, Metacritic Score.

--- app/game-db/components/game-card.js
+++ app/game-db/components/game-card.js
@@ -1,16 +1,30 @@
 export default function GameCard({ game }) {
-  // const { gameID, name, gameIMG, storeID, dealID, salePrice, normalPrice, steamRatingText, steamRatingPercent, steamRatingCount, metacriticScore, metacriticLink } = game;
   return (
     <div className="border border-gray-600 rounded-lg p-4 bg-slate-700">
       <img src={game.gameIMG} alt={game.name} height={100} width={100} />
-      <h2>{game.name}</h2>
+      <h2 className="font-semibold text-slate-200">{game.name}</h2>
       <p>Price: ${game.normalPrice}</p>
-      <p>Sale Price: ${game.salePrice}</p>
       <p>
-        Steam Rating: {game.steamRatingPercent}% ({game.steamRatingText})
+        Sale Price: <span className="text-red-500">${game.salePrice}</span>
       </p>
-      <p>Steam Rating Count: {game.steamRatingCount}</p>
-      <p>Metacritic Score: {game.metacriticScore}</p>
+
+      {/* Steam Rating */}
+      {game.steamRatingPercent > 0 && (
+        <p>
+          Steam Rating: {game.steamRatingPercent}% ({game.steamRatingText})
+        </p>
+      )}
+
+      {/* Steam Rating Count */}
+      {game.steamRatingCount > 0 && (
+        <p>Steam Rating Count: {game.steamRatingCount}</p>
+      )}
+
+      {/* Metacritic Score */}
+      {game.metacriticScore > 0 && (
+        <p>Metacritic Score: {game.metacriticScore}</p>
+      )}
+
       <button className="hover:cursor-pointer">
         {game.isSaved ? "♥ Unfavorite" : "♡ Favorite"}
       </button>
