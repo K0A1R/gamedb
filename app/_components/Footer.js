499f2641ef9d543a95c8602fb22768a1741f140d
Amrit Reddy <amrit_reddy@me.com>
2025-04-14 19:47:56 -0600

    Update footer.js
    Add GitHub link.

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -1,15 +1,25 @@
 export default function Footer() {
   return (
     <footer className="flex justify-center bg-gray-900 text-gray-200 h-16 rounded-md p-4 shadow-sm">
-      <p className="mr-1">Powered by</p>
-      <a
-        href="https://apidocs.cheapshark.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-gray-200 hover:text-blue-300"
-      >
-        CheapShark API
-      </a>
+      <div className="flex">
+        <a
+          href="https://apidocs.cheapshark.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-200 hover:text-blue-300 font-medium"
+        >
+          CheapShark API
+        </a>
+        <span className="text-gray-400 px-2">|</span>
+        <a
+          href="https://github.com/K0A1R/gamedb"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-200 hover:text-blue-300 font-medium"
+        >
+          GitHub
+        </a>
+      </div>
     </footer>
   );
 }
